0385caa24993e599e62b7eb18aa8de71eb28a536
Paco Schwab <Paco.Schwab@live.de>
2023-02-23 12:54:37 +0100

    add answer toggle

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 // phew… not a lot going on here. Please add some code!
 const bookmarkButton = document.querySelector('[data-js="bookmark"]');
 const answerButton = document.querySelector('[data-js="card__button-answer"]');
+const cardAnswer = document.querySelector('[data-js="card__answer"]');
 
 bookmarkButton.addEventListener("click", () => {
   bookmarkButton.classList.toggle("bookmark--active");
+});
+
+/* Alternative
+answerButton.addEventListener("click", () => {
+  cardAnswer.classList.toggle("card__answer--active");
+});
+*/
+
+answerButton.addEventListener("click", () => {
+  if (cardAnswer.hasAttribute("hidden")) {
+    cardAnswer.removeAttribute("hidden");
+  } else {
+    cardAnswer.setAttribute("hidden", "");
+  }
 });
