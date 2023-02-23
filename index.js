bc205805880b1a39138f3066d870c9eb2b0c25d7
Paco Schwab <Paco.Schwab@live.de>
2023-02-23 11:35:40 +0100

    add bookmark toggle

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // phew… not a lot going on here. Please add some code!
+const bookmarkButton = document.querySelector('[data-js="bookmark"]');
+const answerButton = document.querySelector('[data-js="card__button-answer"]');
+
+bookmarkButton.addEventListener("click", () => {
+  bookmarkButton.classList.toggle("bookmark--active");
+});
